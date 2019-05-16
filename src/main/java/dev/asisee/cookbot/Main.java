bc0f2335faf21cd1dc08fe94d156f5b6825c5f43
as-i-see <karlovevheniy@gmail.com>
2019-05-16 15:38:54 +0300

    psycommit

--- src/main/java/dev/asisee/cookbot/Main.java
+++ src/main/java/dev/asisee/cookbot/Main.java
@@ -1,20 +1,24 @@
 package dev.asisee.cookbot;
 
 import dev.asisee.cookbot.bot.CookBot;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
 
-        TelegramBotsApi botsApi = new TelegramBotsApi();
+  public static void main(String[] args) {
+    ApiContextInitializer.init();
 
-        try {
-            botsApi.registerBot(new CookBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+    TelegramBotsApi botsApi = new TelegramBotsApi();
+
+    try {
+      botsApi.registerBot(new CookBot());
+    } catch(TelegramApiException e) {
+      e.printStackTrace();
     }
+  }
+
 }
+
--- src/main/java/dev/asisee/cookbot/bot/CookBot.java
+++ src/main/java/dev/asisee/cookbot/bot/CookBot.java
@@ -1,5 +1,8 @@
 package dev.asisee.cookbot.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -8,54 +11,59 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRem
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
-import java.util.List;
+public class CookBot
+  extends TelegramLongPollingBot {
 
-public class CookBot extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText())
-                    .setReplyMarkup(getRootMenu());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+  @Override
+  public void onUpdateReceived(
+    Update update
+  ) {// We check if the update has a message and the message has text
 
-    @Override
-    public String getBotUsername() {
-        return "simplecookbot";
-    }
+    if (update.hasMessage() && update.getMessage().hasText()) {
+      SendMessage message = new SendMessage(
 
-    @Override
-    public String getBotToken() {
-        return "860409472:AAHhq47abH-VXnwIBpddDasGzs-p8RsSQp8";
+      ).// Create a SendMessage object with mandatory fields
+      setChatId(update.getMessage().getChatId()).setText(
+        update.getMessage().getText()
+      ).setReplyMarkup(getRootMenu());
+      try {
+        execute(message); // Call method to send the message
+      } catch(TelegramApiException e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    private ReplyKeyboardMarkup getRootMenu() {
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        // Create the keyboard (list of keyboard rows)
-        List<KeyboardRow> keyboard = new ArrayList<>();
-        // Create a keyboard row
-        KeyboardRow row = new KeyboardRow();
-        // Set each button, you can also use KeyboardButton objects if you need something else than text
-        row.add("Search for recipe");
-        // Add the first row to the keyboard
-        keyboard.add(row);
-        // Create another keyboard row
-        row = new KeyboardRow();
-        // Set each button for the second line
-        row.add("Goods in stock");
-        // Add the second row to the keyboard
-        keyboard.add(row);
-        // Set the keyboard to the markup
-        keyboardMarkup.setKeyboard(keyboard);
-        return keyboardMarkup;
-    }
+  @Override
+  public String getBotUsername() {
+    return "simplecookbot";
+  }
+
+  @Override
+  public String getBotToken() {
+    return "860409472:AAHhq47abH-VXnwIBpddDasGzs-p8RsSQp8";
+  }
+
+  private ReplyKeyboardMarkup getRootMenu() {
+    ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+    // Create the keyboard (list of keyboard rows)
+    List<KeyboardRow> keyboard = new ArrayList<>();
+    // Create a keyboard row
+    KeyboardRow row = new KeyboardRow();
+    // Set each button, you can also use KeyboardButton objects if you need something else than text
+    row.add("Search for recipe");
+    // Add the first row to the keyboard
+    keyboard.add(row);
+    // Create another keyboard row
+    row = new KeyboardRow();
+    // Set each button for the second line
+    row.add("Goods in stock");
+    // Add the second row to the keyboard
+    keyboard.add(row);
+    // Set the keyboard to the markup
+    keyboardMarkup.setKeyboard(keyboard);
+    return keyboardMarkup;
+  }
+
 }
+
