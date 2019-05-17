b4c52e7f20bff287851bd494b18e64b558396ab1
as-i-see <karlovevheniy@gmail.com>
2019-05-17 07:55:20 +0300

    Heroku Redis migration

--- src/main/java/dev/asisee/cookbot/bot/CookBot.java
+++ src/main/java/dev/asisee/cookbot/bot/CookBot.java
@@ -1,89 +1,90 @@
 package dev.asisee.cookbot.bot;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CookBot
-  extends TelegramLongPollingBot {
-  static final JedisPool pool = new JedisPool(
-    new JedisPoolConfig(),
-    "redis-13478.c3.eu-west-1-2.ec2.cloud.redislabs.com", 13478, 1000, "O8fM207KvSYR4OzH473Lp6DUUkregDms"
-  );
+        extends TelegramLongPollingBot {
+    private static final URI cloudRedisURI = URI.create("redis://h:p4426bdca631eb9bff2f2ce99c4799ede68fb59900642e47783781d87a5986cb2@ec2-99-80-37-150.eu-west-1.compute.amazonaws.com:17669");
+    static final JedisPool pool = new JedisPool(
+            new JedisPoolConfig(), CookBot.cloudRedisURI
+    );
 
-  @Override
-  public void onClosing() {
-    super.onClosing();
-    pool.close();
-  }
-
-  @Override
-  public void onUpdateReceived(Update update) {
-    if (update.hasMessage() && update.getMessage().hasText()) {
-      StringBuilder stringBuilder = new StringBuilder(
-        "You have already sent:\n"
-      );
-      try (Jedis jedis = pool.getResource()){
-        jedis.zadd(
-          update.getMessage().getChatId().toString(),
-          0,
-          update.getMessage().getText()
-        );
-        jedis.zrange(update.getMessage().getChatId().toString(), 0, -1).forEach(
-          stringBuilder::append
-        );
-      }
-      SendMessage message = new SendMessage().setChatId(
-        update.getMessage().getChatId()
-      ).setText(stringBuilder.toString()).setReplyMarkup(getRootMenu());
-      try {
-        execute(message); // Call method to send the message
-      } catch(TelegramApiException e) {
-        e.printStackTrace();
-      }
+    @Override
+    public void onClosing() {
+        super.onClosing();
+        CookBot.pool.close();
     }
-  }
 
-  @Override
-  public String getBotUsername() {
-    return "simplecookbot";
-  }
+    @Override
+    public void onUpdateReceived(final Update update) {
+//    Message message = update.getMessage();
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            final StringBuilder stringBuilder = new StringBuilder(
+                    "You have already sent:\n"
+            );
+            try (final Jedis jedis = CookBot.pool.getResource()) {
+                jedis.zadd(
+                        update.getMessage().getChatId().toString(),
+                        0,
+                        update.getMessage().getText()
+                );
+                jedis.zrange(update.getMessage().getChatId().toString(), 0, -1).forEach(
+                        stringBuilder::append
+                );
+            }
+            final SendMessage message = new SendMessage().setChatId(
+                    update.getMessage().getChatId()
+            ).setText(stringBuilder.toString()).setReplyMarkup(this.getRootMenu());
+            try {
+                this.execute(message); // Call method to send the message
+            } catch (final TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-  @Override
-  public String getBotToken() {
-    return "860409472:AAHhq47abH-VXnwIBpddDasGzs-p8RsSQp8";
-  }
+    @Override
+    public String getBotUsername() {
+        return "simplecookbot";
+    }
 
-  private ReplyKeyboardMarkup getRootMenu() {
-    ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-    // Create the keyboard (list of keyboard rows)
-    List<KeyboardRow> keyboard = new ArrayList<>();
-    // Create a keyboard row
-    KeyboardRow row = new KeyboardRow();
-    // Set each button, you can also use KeyboardButton objects if you need something else than text
-    row.add("Search for recipe");
-    // Add the first row to the keyboard
-    keyboard.add(row);
-    // Create another keyboard row
-    row = new KeyboardRow();
-    // Set each button for the second line
-    row.add("Goods in stock");
-    // Add the second row to the keyboard
-    keyboard.add(row);
-    // Set the keyboard to the markup
-    keyboardMarkup.setKeyboard(keyboard);
-    return keyboardMarkup;
-  }
+    @Override
+    public String getBotToken() {
+        return "860409472:AAHhq47abH-VXnwIBpddDasGzs-p8RsSQp8";
+    }
+
+    private ReplyKeyboardMarkup getRootMenu() {
+        final ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        // Create the keyboard (list of keyboard rows)
+        final List<KeyboardRow> keyboard = new ArrayList<>();
+        // Create a keyboard row
+        KeyboardRow row = new KeyboardRow();
+        // Set each button, you can also use KeyboardButton objects if you need something else than text
+        row.add("Search for recipe");
+        // Add the first row to the keyboard
+        keyboard.add(row);
+        // Create another keyboard row
+        row = new KeyboardRow();
+        // Set each button for the second line
+        row.add("Goods in stock");
+        // Add the second row to the keyboard
+        keyboard.add(row);
+        // Set the keyboard to the markup
+        keyboardMarkup.setKeyboard(keyboard);
+        return keyboardMarkup;
+    }
 
 }
 
